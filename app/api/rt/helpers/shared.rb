6576972543eeb20747ba3c337655ca09f77253e9
sandri4 <pletin1997@outlook.com>
2021-05-28 02:19:45 +0300

    fix api authorization

---
+++ app/api/rt/helpers/shared.rb
@@ -0,0 +1,26 @@
+module Rt
+  module Helpers
+    module Shared
+      attr_reader :current_user
+
+      def declared_params
+        declared(params, include_missing: false).with_indifferent_access
+      end
+
+      def authenticate
+        _, headers_token = headers['Authorization'].split
+
+        token, = JWT.decode  headers_token, ENV.fetch('AUTH_SECRET'), true, { algorithm: 'HS256' }
+
+        @current_user = User.find(token['user_id'])
+      rescue JWT::DecodeError
+        error_response = { error: I18n.t('devise.failure.unauthenticated') }
+        error!(error_response, 401)
+      end
+
+      def present_validation_error_for(record)
+        present error: record.errors.messages
+      end
+    end
+  end
+end
